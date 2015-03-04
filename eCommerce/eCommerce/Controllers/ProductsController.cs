f60c8355b23222ed97f3a8467534c250537e2269
Dustin Kraft <kraftdr@gmail.com>
2015-03-03 17:21:18 -0700

    init checkin

---
+++ eCommerce/eCommerce/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //this is our data context, think of it as the gateway to the database.  it's how we store and retrieve information from the database.
+        private Models.productsDatabaseEntities db = new Models.productsDatabaseEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            //return all products
+            return View(db.Products);
+        }
+
+        public ActionResult Details(int id)
+        {
+            //return just the one product the user wants to see
+            var theProduct = db.Products.Find(id);
+            return View(theProduct);
+        }
+    }
+}
